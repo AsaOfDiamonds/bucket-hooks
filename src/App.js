138eed7454d69b74e3def612353ffa6ba4585eb5
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-09 16:13:15 -0600

    Started App, created state in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const [bucket, setBucket] = useState({
+    bucket: [
+      {
+        name: 'Go to Ukraine',
+        id: 122,
+        completed: false
+      },
+      {
+        name: 'Find other half',
+        id: 123,
+        completed: false
+      },
+      {
+        name: 'Live happily ever after',
+        id: 124,
+        completed: false
+      }
+    ]
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bucket List with Hooks</h1>
       </header>
     </div>
   );
